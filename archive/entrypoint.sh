1babe884ba234b7da0d59091c5c110ee3d488d95
Ajit Bohra <ajit@lubus.in>
2019-01-03 12:03:18 +0530

    Cleanup archive action

--- archive/entrypoint.sh
+++ archive/entrypoint.sh
@@ -4,8 +4,8 @@
 set -eu
 
 # Create zip archive
-echo "\n⏳ Creating zip archive\n"
+echo "\n📦 Creating zip archive\n"
 
-zip -r "$*.zip" . -x '/*node_modules/*' '/*.git/*' '/*.github/*' '/*.wordpress-org/*' '/*src/*' '/*.gitkeep' '/*.distignore' '/*.gitignore' '/*.editorconfig' '/*.eslintignore' '/*.eslintrc' '/*.nvmrc' '/*CHANGELOG.md' '/*CONTRIBUTING.md' '/*docker-compose.yml' '/*package-lock.json' '/*package.json' '/*README.md' '/*webpack.config.js'
+zip -r "${ARCHIVE_FILENAME}.zip" . -x '/*node_modules/*' '/*.git/*' '/*.github/*' '/*.wordpress-org/*' '/*src/*' '/*.gitkeep' '/*.distignore' '/*.gitignore' '/*.editorconfig' '/*.eslintignore' '/*.eslintrc' '/*.nvmrc' '/*CHANGELOG.md' '/*CONTRIBUTING.md' '/*docker-compose.yml' '/*package-lock.json' '/*package.json' '/*README.md' '/*webpack.config.js' '/*actions/*'
 
-echo "\n✅ All done 👌\n"
+echo "\n🚀 All right sparky ✔️\n"
